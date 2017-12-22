e3839d3580dc1cbc64b25500bd8b45eeca35e150
Michael Andersen <michael@steelcode.com>
2017-12-22 10:15:28 -0800

    Add stub

--- src/algorithm.go
+++ src/algorithm.go
@@ -1,12 +1,47 @@
 package main
 
 import (
+	"fmt"
+	"math"
+	"time"
+
+	"github.com/davecgh/go-spew/spew"
 	l7g "github.com/immesys/chirp-l7g"
 )
 
+//Notes
+/*
+- you only calibrate once, and you seem to require some special
+  setup while calibrating
+- you don't properly handle missing data, you treat all readings
+  as next to each other in time
+*/
+
+//You can put some state variables and type definitions here
+type path struct {
+	Src int
+	Dst int
+}
+type algorithmstate struct {
+	//This is just example information copied from python
+	AnemometerID       string
+	IsDuct             bool
+	CalibrationPeriod  time.Duration
+	IsCalibrating      bool
+	PrevRelPhase       map[path]float64
+	PrevAbsPhase       map[path]float64
+	CurAbsPhase        map[path]float64
+	CalibrationPhases  map[path][]float64
+	CalibrationIndexes map[path][]int
+	CalibratedIndex    map[path]int
+	Paths              []path
+}
+
+var states map[string]*algorithmstate
+
 func main() {
 	//Register and run our algorithm.
-	l7g.RunDPA([]byte(ourEntity), Initialize, OnNewData,
+	l7g.RunDPA([]byte(ourEntity), Initialize, OnNewDataSet,
 		//This is the algorithm vendor
 		"ucberkeley",
 		//This is the algorithm version
@@ -14,125 +49,109 @@ func main() {
 }
 
 func Initialize(emit l7g.Emitter) {
-	//We actually do not do any initialization in this implementation, but if
-	//you want to, you can do it here.
+	states = make(map[string]*algorithmstate)
 }
 
-// OnNewData encapsulates the algorithm. You can store the emitter and
+// OnNewDataSet encapsulates the algorithm. You can store the emitter and
 // use it asynchronously if required. You can see the documentation for the
 // parameters at https://godoc.org/github.com/immesys/chirp-l7g
-func OnNewData(popHdr *l7g.L7GHeader, h *l7g.ChirpHeader, emit l7g.Emitter) {
-	/*
-		// Define some magic constants for the algorithm
-		magic_count_tx := -4
+// The popHdr and data arrays are always length 4 and represent the data
+// for four sets of readings with primary = 0..3. They are always contiguous
+// in time. If the set is incomplete due to data loss, the missing elements
+// will have nil in them.
+// Sometimes the popHdr will be nil for an element even if the data is present
+// this indicates a packet that was lost but reconstructed using forward
+// error correction codes.
+func OnNewDataSet(info l7g.SetInfo, popHdr []*l7g.L7GHeader, data []*l7g.ChirpHeader, emit l7g.Emitter) {
 
-		// Create our output data set. For this reference implementation,
-		// we emit one TOF measurement for every raw TOF sample (no averaging)
-		// so the timestamp is simply the raw timestamp obtained from the
-		// Border Router. We also identify the sensor simply from the mac address
-		// (this is fine for most cases)
-		odata := l7g.OutputData{
-			Timestamp: popHdr.Brtime,
-			Sensor:    popHdr.Srcmac,
+	//We only want to process complete sets of data
+	if !info.Complete {
+		return
+	}
+
+	//This string is the complete ID of this anemometer
+	//You can use this as a key into buffers of historic state
+	idstring := info.Site + "_" + info.MAC
+	state, found := states[idstring]
+	if !found {
+		//Initialize new algorithm state here
+		//perhaps make it calibrate or something
+		state = &algorithmstate{
+			AnemometerID: idstring,
+			IsDuct:       info.IsDuct(),
 		}
+		states[idstring] = state
+	}
 
-		// For each of the four measurements in the data set
-		for set := 0; set < 4; set++ {
-			// For now, ignore the data read from the ASIC in TXRX
-			if int(h.Primary) == set {
+	//Initialize the output data object
+	//We will build up the data inside this as we process the input data
+	outputdata := l7g.OutputData{
+		Timestamp: info.TimeOfFirst,
+		Sensor:    idstring,
+	}
+
+	//Over all paths
+	for src := 0; src < 4; src++ {
+		for dst := 0; dst < 4; dst++ {
+			if src == dst {
+				//We don't use data from primary
 				continue
 			}
-
-			// alias the data for readability. This is the 70 byte dataset
-			// read from the ASIC
-			data := h.Data[set]
-
-			//The first six bytes of the data
-			tof_sf := binary.LittleEndian.Uint16(data[0:2])
-			tof_est := binary.LittleEndian.Uint16(data[2:4])
-			intensity := binary.LittleEndian.Uint16(data[4:6])
-
-			//Load the complex numbers
-			iz := make([]int16, 16)
-			qz := make([]int16, 16)
-			for i := 0; i < 16; i++ {
-				qz[i] = int16(binary.LittleEndian.Uint16(data[6+4*i:]))
-				iz[i] = int16(binary.LittleEndian.Uint16(data[6+4*i+2:]))
+			p := path{src, dst}
+			maxIndex := data[src].MaxIndex[dst]
+			if maxIndex < 3 {
+				//This might indicate something is wrong
+				//It also means that our IQ values start from 0
+				//not from maxIndex-3
 			}
+			Ivalue2beforeMax := data[src].IValues[dst][1] //0 is 3 before
+			Qvalue2beforeMax := data[src].QValues[dst][1]
 
-			//Find the largest complex magnitude (as a square). We do this like this
-			//because it more closely mirror how it would be done on an embedded device
-			// (actually because I copied the known-good firestorm implementation)
-			magsqr := make([]uint64, 16)
-			magmax := uint64(0)
-			for i := 0; i < 16; i++ {
-				magsqr[i] = uint64(int64(qz[i])*int64(qz[i]) + int64(iz[i])*int64(iz[i]))
-				if magsqr[i] > magmax {
-					magmax = magsqr[i]
-				}
-			}
+			//Use this as well as other data
+			_ = p
+			_ = maxIndex
+			_ = Ivalue2beforeMax
+			_ = Qvalue2beforeMax
 
-			//Find the first index to be greater than half the max (quarter the square)
-			quarter := magmax / 4
-			less_idx := 0
-			greater_idx := 0
-			for i := 0; i < 16; i++ {
-				if magsqr[i] < quarter {
-					less_idx = i
-				}
-				if magsqr[i] > quarter {
-					greater_idx = i
-					break
-				}
-			}
-
-			//Convert the squares into normal floating point
-			less_val := math.Sqrt(float64(magsqr[less_idx]))
-			greater_val := math.Sqrt(float64(magsqr[greater_idx]))
-			half_val := math.Sqrt(float64(quarter))
-			//CalPulse is in microseconds
-			freq := float64(tof_sf) / 2048 * float64(h.CalRes[set]) / (float64(h.CalPulse) / 1000)
-			//Linearly interpolate the index (the index is related to time of flight because it is regularly sampled)
-			lerp_idx := float64(less_idx) + (half_val-less_val)/(greater_val-less_val)
-			//Fudge the result with magic_count_tx and turn into time of flight
-			//The *2 here is debugging, its not real
-			tof := (lerp_idx + float64(magic_count_tx)) / freq * 8 * 2
-
-			//We print these just for fun / debugging, but this is not actually emitting the data
-			fmt.Printf("SEQ %d ASIC %d primary=%d\n", h.Seqno, set, h.Primary)
-			fmt.Println("lerp_idx: ", lerp_idx)
-			fmt.Println("tof_sf: ", tof_sf)
-			fmt.Println("freq: ", freq)
-			fmt.Printf("tof: %.2f us\n", tof*1000000)
-			fmt.Println("intensity: ", intensity)
-			fmt.Println("tof chip estimate: ", tof_est)
-			fmt.Println("tof 50us estimate: ", lerp_idx*50)
-			fmt.Println("data: ")
-			for i := 0; i < 16; i++ {
-				fmt.Printf(" [%2d] %6d + %6di (%.2f)\n", i, qz[i], iz[i], math.Sqrt(float64(magsqr[i])))
-			}
-			fmt.Println(".")
-
-			//Append this time of flight to the output data set
-			//For more "real" implementations, this would likely
-			//be a rolling-window smoothed time of flight. You do not have
-			//to base this value on just the data from this set and
-			//you do not have to emit every time either (downsampling is ok)
-			odata.Tofs = append(odata.Tofs, l7g.TOFMeasure{
-				Src: int(h.Primary),
-				Dst: set,
-				Val: tof * 1000000,
+			//To populate outputdata
+			outputdata.Tofs = append(outputdata.Tofs, l7g.TOFMeasure{
+				Src: src,
+				Dst: dst,
+				//Here you should actually put the time of flight in microseconds
+				Val: 555,
 			})
-		} //end for each of the four measurements
+			outputdata.Temperatures = append(outputdata.Temperatures, l7g.TempMeasure{
+				Src: src,
+				Dst: dst,
+				//Here you should actually put the temperature in celsius
+				Val: 25.5,
+			})
+		}
+	}
 
-		// Now we would also emit the velocities. I imagine this would use
-		// the averaged/corrected time of flights that are emitted above
-		// (when they are actually averaged/corrected)
-		// For now, just a placeholder
-		odata.Velocities = append(odata.Velocities, l7g.VelocityMeasure{X: 42, Y: 43, Z: 44})
+	//If you have a new velocity estimate, you should also add that here
+	//These are in m/s
+	outputdata.Velocities.Valid = true
+	outputdata.Velocities.X = 1 //due north
+	outputdata.Velocities.Y = 2 //due west
+	outputdata.Velocities.Z = 3 //vertical
+	//Also in m/s
+	outputdata.Velocities.Mag = math.Sqrt(1 + 4 + 9)
+	// Phi, the azimuthal angle is the degrees counterclockwize from North (X)
+	outputdata.Velocities.Phi = 55
+	// Theta, the polar angle is the degrees from vertical
+	outputdata.Velocities.Theta = 66
 
-		//Emit the data on the SASC bus
-		emit.Data(odata)
+	//If you have other information output from the algorithm you can
+	//add that in here
+	outputdata.Extradata = append(outputdata.Extradata, "we are busy calibrating")
 
-	*/
+	//Note that you can also use print statements. These are not visible to
+	//consumers of the data, but you can see them using `make watch`
+	fmt.Printf("About to emit data\n")
+	spew.Dump(outputdata) //this prints the whole object nicely
+
+	//Emit the data on the data bus
+	emit.Data(outputdata)
+
 }
